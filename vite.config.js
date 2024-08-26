a5ec114bc88d13754f689778b7f12158c84786ed
Aq202 <diegoguatedb2002@gmail.com>
2024-08-25 19:36:26 -0600

    fix: vite configuration for btoa

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     'process.env': {},
     global: {},
     "global.WebSocket": "window.WebSocket",
+    "global.btoa": "window.btoa.bind(window)",
   },
   optimizeDeps: {
     esbuildOptions: {
